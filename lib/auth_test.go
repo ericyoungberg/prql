04fd220997ca0d84c5d37cee904ec17942d81453
Eric Youngberg <eyoungberg@mapc.org>
2019-02-03 09:37:56 -0500

    Test auth

--- lib/auth_test.go
+++ lib/auth_test.go
@@ -12,17 +12,17 @@ const (
 )
 
 var (
-  testStrings = [4]string{
-    "a key", 
-    "testkey2", 
-    "Streaming a bad dystopian movie right now", 
-    "areallylongkeythatwillreallypushthelimitsbeyondrealworldusecases",
+  testStrings = map[string]string{
+    "a key": "ec4f6067|32e9ea4aae54cf8d195f6b34edc3f4cc30994d164d005b0150cfe199a0aaddbfe0",
+    "testkey2": "a2e27a3d|e592bbc3d8e1efac90223630f29baedf327b4a198abd738b1e702fadc58630872288df85", 
+    "Streaming a bad dystopian movie right now": "7bbf55de|cba5f9822499f2eac97f2cc957da2b3ca56c0d8fbae8908c1e44dc6b89db4f9d6867b5fb179780f423bb2ac3e89af76f66f39ae94caef6cbc7ed20d65787c251a642850075", 
+    "areallylongkeythatwillreallypushthelimitsbeyondrealworldusecases": "aa571bac|470178ce24bfc4584ee54f9934d5ea0c17757f798a03be535f53b4a88209145e2dd29a2d218f9c2fb6bdb5db1baee94f0cfaf937ecd3d960ba9211ca0c9b78599a6b7a20f219d88577ef6bf29f21bd38ca4b2ce0a3095e201d4359a7",
   }
 )
 
 
 func TestInsecureCryptography(t *testing.T) {
-  for _, data := range testStrings {
+  for data, _ := range testStrings {
     encrypted := InsecureEncryptString(data)
     decrypted, err := InsecureDecryptString(encrypted)
 
@@ -37,8 +37,14 @@ func TestInsecureCryptography(t *testing.T) {
 }
 
 func TestInsecureDecryptString(t *testing.T) {
-  for _, data := range testStrings {
-    
+  for key, encrypted := range testStrings {
+    decrypted, err := InsecureDecryptString(encrypted)
+
+    if err != nil {
+      t.Error(err) 
+    } else if decrypted != key {
+      t.Errorf("expected decrypted data to be %s, was %s [value=%s]", key, decrypted, encrypted) 
+    }
   } 
 }
 
@@ -46,7 +52,7 @@ func TestInsecureEncryptString(t *testing.T) {
   encryptionFmt := fmt.Sprintf("^\\w{%d,}\\%s\\w+$", minSaltLength, saltDelimiter)
   encryptionRegex, _ := regexp.Compile(encryptionFmt)
 
-  for _, data := range testStrings {
+  for data, _ := range testStrings {
     encryptedData := InsecureEncryptString(data)
 
     if !encryptionRegex.MatchString(encryptedData) {
@@ -64,7 +70,7 @@ func TestInsecureEncryptString(t *testing.T) {
 func TestCreateHash(t *testing.T) {
   hashes := make(map[string]string)
 
-  for _, key := range testStrings {
+  for key, _ := range testStrings {
     hash := CreateHash(key)
     
     if hash == key {
